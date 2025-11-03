1c2bab70b5ef87afb02af2855e04fc0c8fb7f0e5
avinav77 <avinavjohari7@gmail.com>
2025-11-03 12:59:59 +0530

    bookupdated

--- Book-App/book/src/App.jsx
+++ Book-App/book/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Book from './components/Book.jsx'
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <div>
+      <Navbar/>
+      <Book/>
+      <Book/>
+      <Book/>
       <Book/>
     </div>
   )
---
+++ Book-App/book/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <ul id='nav'>
+            <li><img src="https://logowik.com/content/uploads/images/chef-restaurant5078.logowik.com.webp" alt="Logo" height={50} /></li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact us</li>
+            <li>Menu</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
