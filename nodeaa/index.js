244bc8c10cbcaf248a2373df79996ebcb8c5c424
avinav77 <avinavjohari7@gmail.com>
2026-02-23 13:05:46 +0530

    express

--- nodeaa/index.js
+++ nodeaa/index.js
@@ -1,9 +1,59 @@
-const os= require('os');
-console.log('platform:', os.platform());
-console.log('userInfo:', os.userInfo());
-console.log('cpus:', os.arch());
-console.log('free memory:', os.freemem());
-console.log('memory:', os.totalmem());
-console.log('time',os.uptime());
-console.log('homedir',os.homedir());
-console.log('host-name',os.hostname());
+// const http=require('http');
+// const myserver=http.createServer((req,res)=>{
+//     if(req.url=='/'){
+//         res.end('welcome to home page');
+//     }
+//     else if(req.url=='/about'){
+//         res.end(`<h1>abes engineering college</h1>
+//             <img src="https://unsplash.com/photos/a-dark-grey-sports-car-with-red-wheels-hSCrpBasAss">
+//         `);
+//     }
+//     else if(req.url=='/contact us'){
+//         res.end('1234567890');
+//     }
+//     else if(req.url=='/class'){
+//         res.end('I.T-A');
+//     }
+//     else{
+//         res.end('404 page not found');
+//     }
+// });
+// myserver.listen(8000,(req,res)=>{
+//     console.log("server is running on port 8000");
+// })
+
+// constfs=require('fs');
+// fs.writeFileSync("./it-a.txt","we are student of i.t-a class");
+// fs.writeFileSync("./abes.txt","we are student of abes");
+
+
+// const fs=require('fs');
+// fs.writeFile("./it-b.txt","we are student of  it-a class",()=>{});
+// fs.readFile("./it-b.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("error",err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
+
+// fs.appendFileSync("./it-b.txt",'and i am sad');
+// fs.appendFile("./it-b.txt",'hello hii bye ')
+
+
+
+// const fs=require('fs');
+// fs.writeFileSync("./a1.txt","we are student of i.t-a class");
+// fs.cpSync("./a1.txt","./b1.txt");
+// fs.unlinkSync("./a1.txt");
+
+
+const os=require('os');
+console.log("platform",os.platform());
+console.log("userinfo",os.userInfo());
+console.log("arch",os.arch());
+console.log("freememory",os.freemem());
+console.log("totalmem",os.totalmem());
+console.log("time",os.uptime());
+console.log(os.hostname());
--- nodeaa/server.js
+++ nodeaa/server.js
@@ -1,11 +1,20 @@
-const http = require('http');
-const fs = require('fs');
-const abes = fs.readFileSync("abes.html");
+// const http = require('http');
+// const fs = require('fs');
+// const abes = fs.readFileSync("abes.html");
+// const myServer=http.createServer((req,res)=>{
+//     res.statusCode=200;
+//     res.setHeader("Content-Type","text/html");
+//     res.end(abes);
+// });
+// myServer.listen(5500,(req,res)=>{
+// console.log("server is running on port 5500");
+// });
+
+const http= require('http');
+const fs= require('fs');
+const abesa= fs.readFileSync("abes.html");
 const myServer=http.createServer((req,res)=>{
     res.statusCode=200;
-    // res.setHeader("Content-Type","text/html");
-    res.end(abes);
-});
-myServer.listen(5500,(req,res)=>{
-console.log("server is running on port 5500");
+    res.setHeader("Content-Type","text/html");
+    res.end(abesa);
 });
